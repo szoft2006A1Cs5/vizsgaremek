0ff1299b83ab466f57cbaa479fa2df4f145fcf74
Bence Megyeri <megyeri.bence@hbsz.edu.hu>
2026-03-17 08:21:53 +0100

    Toltsd fel lien!!!!!!4á

--- CoMove/src/components/Account/Account.jsx
+++ CoMove/src/components/Account/Account.jsx
@@ -1,5 +1,5 @@
 import "./Account.css";
-import logo from "../../assets/kepek/logo/comove_logo1.png";
+import logo from "../../assets/kepek/logo/comove_logo4.png";
 import { useLayoutEffect, useEffect, useState } from "react";
 
 function Account() {
@@ -20,16 +20,25 @@ function Account() {
         className={`account_hero account_pageAnim ${loaded ? "isLoaded" : ""}`}
       >
         <img className="account_logo" src={logo} alt="CoMove" />
-        <h1>Fiókbeállítások</h1>
+        <h1 className="account_focim">Fiókbeállítások</h1>
+        <p className="account_alcim">Módosítsa adatait</p>
       </section>
 
       <section
         className={`account_page account_pageAnim ${loaded ? "isLoaded" : ""}`}
       >
         <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
-          {/* IDE JÖN MAJD A PROFIL */}
           <div>
-            <div className="map"></div>
+            <div className="map">
+              <p className="account_tag">Teljes Név</p>
+              <input type="text" placeholder="Kiss Károly" />
+
+              <p className="account_tag">Teljes Név</p>
+              <input type="text" />
+
+              <p className="account_tag">Teljes Név</p>
+              <input type="text" />
+            </div>
           </div>
         </div>
       </section>
